d323fbf60853303e408cb743d35e15015f4b2b50
xsine <neoway@126.com>
2022-03-28 06:44:15 +0800

    init

---
+++ mcu_libs/lib/lib_config.c
@@ -0,0 +1,4 @@
+//
+// Created by master on 2022/3/28.
+//
+
---
+++ mcu_libs/lib/lib_config.h
@@ -0,0 +1,8 @@
+//
+// Created by master on 2022/3/28.
+//
+
+#ifndef MCU_LIB_CONFIG_H
+#define MCU_LIB_CONFIG_H
+
+#endif //MCU_LIB_CONFIG_H
