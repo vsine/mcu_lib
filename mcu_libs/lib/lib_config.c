b7c4378c48e3ecbfbcb1522e2b3b2265c000a0a3
xsine <neoway@126.com>
2022-03-28 12:07:38 +0800

    init

--- mcu_libs/lib/lib_config.c
+++ mcu_libs/lib/lib_config.c
@@ -14,11 +14,3 @@ char u8g2_spi_send(uint8_t arg_int,void *arg_ptr){
 }
 #endif
 
-char u8g2_spi_send(uint8_t arg_int,void *arg_ptr){
-#ifdef STM32_HAL_VERSION
-    if(HAL_SPI_Transmit(&U8G2_SPI,arg_ptr,arg_int,1000)==HAL_OK)
-        return 1;
-    else
-        return 0;
-#endif
-}
--- mcu_libs/lib/lib_config.h
+++ mcu_libs/lib/lib_config.h
@@ -6,28 +6,21 @@
 #define MCU_LIB_CONFIG_H
 #include "../hal/Inc/posix.h"
 
-//#define USE_U8G2_LIB_SPI
-//char u8g2_spi_send(uint8_t arg_int,void *arg_ptr);
-////u8g2
-//#ifdef  USE_U8G2_LIB_SPI
-//#define OLED_DC_GPIO_Port 1
-//#define OLED_DC_Pin 0
-//#define OLED_RST_GPIO_Port 1
-//#define OLED_RST_Pin 1
-//#ifdef STM32_HAL_VERSION
-//#define U8G2_SPI hspi1
-//extern SPI_HandleTypeDef U8G2_SPI;
-//#endif
-//char u8g2_spi_send(uint8_t arg_int,void *arg_ptr);
-//#endif
+#define USE_U8G2_LIB_SPI
 
-//end u8g2
-#define U8G2_SPI hspi1
-extern SPI_HandleTypeDef U8G2_SPI;
+//u8g2
+#ifdef  USE_U8G2_LIB_SPI
 #define OLED_DC_GPIO_Port 1
 #define OLED_DC_Pin 0
 #define OLED_RST_GPIO_Port 1
 #define OLED_RST_Pin 1
+#ifdef STM32_HAL_VERSION
+#define U8G2_SPI hspi1
+extern SPI_HandleTypeDef U8G2_SPI;
+#endif
 char u8g2_spi_send(uint8_t arg_int,void *arg_ptr);
+#endif
+//end u8g2
+
 
 #endif //MCU_LIB_CONFIG_H
