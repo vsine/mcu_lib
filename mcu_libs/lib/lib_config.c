886273407b7e2daffd7d7be77a6aae0448a76248
xsine <neoway@126.com>
2022-03-28 10:03:05 +0800

    init

--- mcu_libs/lib/lib_config.c
+++ mcu_libs/lib/lib_config.c
@@ -4,7 +4,6 @@
 #include "lib_config.h"
 
 #ifdef  USE_U8G2_LIB_SPI
-
 char u8g2_spi_send(uint8_t arg_int,void *arg_ptr){
     #ifdef STM32_HAL_VERSION
     if(HAL_SPI_Transmit(&U8G2_SPI,arg_ptr,arg_int,1000)==HAL_OK)
@@ -13,5 +12,4 @@ char u8g2_spi_send(uint8_t arg_int,void *arg_ptr){
         return 0;
     #endif
 }
-
 #endif
