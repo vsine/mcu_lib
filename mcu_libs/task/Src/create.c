a58b5ed76ef2fbe3a128691cd5fb547676725fba
xsine <neoway@126.com>
2022-03-28 12:26:47 +0800

    init

--- mcu_libs/task/Src/create.c
+++ mcu_libs/task/Src/create.c
@@ -11,20 +11,23 @@ u8g2_t u8g2;
 void create(){
     HAL_Delay(100);
     u8g2Init(&u8g2);
+    u8g2_SetFont(&u8g2, u8g2_font_10x20_mf);
     u8g2_ClearBuffer(&u8g2);
     u8g2_DrawLine(&u8g2,50,20,60,19);
     u8g2_SendBuffer(&u8g2);
     //HAL_Delay(1000);
 }
-
+char  t=0;
 void loop(){
-    soft_delay(1000);
-    digitalWrite(2,13,1);
-    soft_delay(1000);
-    digitalWrite(2,13,0);
+
     HAL_Delay(100);
-    u8g2Init(&u8g2);
     u8g2_ClearBuffer(&u8g2);
-    u8g2_DrawLine(&u8g2,50,20,60,19);
+    u8g2_DrawStr(&u8g2,20,50,"sce");
+    if(++t >= 32) t = 1;
+    u8g2_DrawCircle(&u8g2,64,32,t,U8G2_DRAW_ALL);
+    u8g2_DrawCircle(&u8g2,32,32,t,U8G2_DRAW_ALL);
+    u8g2_DrawCircle(&u8g2,96,32,t,U8G2_DRAW_ALL);
     u8g2_SendBuffer(&u8g2);
+
+
 }
